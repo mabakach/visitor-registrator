3954cb7fc6bf43d45e392f76c567d36e7b5c0ea2
Marc Baumgartner <marc@mabaka.ch>
2020-05-27 21:13:09 +0200

    Bootstrap syled navigation bar and views for events, about and error added.

--- webapp/visitor-registrator-ui/src/App.js
+++ webapp/visitor-registrator-ui/src/App.js
@@ -1,56 +1,31 @@
-import React from 'react';
-import { MemoryRouter, Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
-import { LinkContainer } from 'react-router-bootstrap';
+import Home from './components/Home';
+import About from './components/About';
+import Events from './components/Events';
+import Error from './components/Error';
+import NavigationBar from './components/NavigationBar';
 
 import './App.css';
 
-const Home = () => <span>Home</span>;
-
-const About = () => <span>About</span>;
-
-const Users = () => <span>Users</span>;
-
-const App = () => (
-  <MemoryRouter>
-    <Container className="p-3">
-      <Jumbotron>
-        <h1 className="header">Welcome To React-Bootstrap</h1>
-        <h2>
-          Current Page is{' '}
-          <Switch>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/users">
-              <Users />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </h2>
-        <h2>
-          Navigate to{' '}
-          <ButtonToolbar className="custom-btn-toolbar">
-            <LinkContainer to="/">
-              <Button>Home</Button>
-            </LinkContainer>
-            <LinkContainer to="/about">
-              <Button>About</Button>
-            </LinkContainer>
-            <LinkContainer to="/users">
-              <Button>Users</Button>
-            </LinkContainer>
-          </ButtonToolbar>
-        </h2>
-      </Jumbotron>
-    </Container>
-  </MemoryRouter>
-);
-
-export default App;
+class App extends Component {
+  render() {
+    return (      
+       <BrowserRouter>
+        <div>
+          <NavigationBar />
+            <Switch>
+             <Route path="/" component={Home} exact/>
+             <Route path="/events" component={Events}/>
+             <Route path="/about" component={About}/>
+            <Route component={Error}/>
+           </Switch>
+        </div> 
+      </BrowserRouter>
+    );
+  }
+}
+ 
+export default App

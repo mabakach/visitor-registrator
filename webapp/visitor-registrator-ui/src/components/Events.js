ac419b496e825bec5cf70a5012999851902d50cd
Marc Baumgartner <marc@mabaka.ch>
2020-06-11 16:44:55 +0200

    Add a 'New Event' Button to the top of the events table

--- webapp/visitor-registrator-ui/src/components/Events.js
+++ webapp/visitor-registrator-ui/src/components/Events.js
@@ -1,37 +1,38 @@
 import React, { Component } from 'react';
 import DataTable from 'react-data-table-component';
+import { Button } from 'react-bootstrap';
  
 const columns2 = [
-	  {
-	    name: 'ID',
-	    selector: 'sysid',
-	    sortable: true,
-	    compact: true,
-	  },
-	  {
-	    name: 'Insert timestamp',
-	    selector: 'sysinsertts',
-	    sortable: true,
-	    compact: true,
-	  },
-	  {
-	    name: 'Event name',
-	    selector: 'name',
-	    sortable: true,
-	  },
-	  {
-	    name: 'Number of seats',
-	    selector: 'numberOfSeats',
-	    sortable: true,
-	    right: true,
-	  },
-	  {
-	    name: 'Start time',
-	    selector: 'startDateTime',
-	    sortable: true,
-	    compact: true,
-	  },
-	  
+	{
+	name: 'ID',
+	selector: 'sysid',
+	sortable: true,
+	compact: true,
+	},
+	{
+	name: 'Insert timestamp',
+	selector: 'sysinsertts',
+	sortable: true,
+	compact: true,
+	},
+	{
+	name: 'Event name',
+	selector: 'name',
+	sortable: true,
+	},
+	{
+	name: 'Number of seats',
+	selector: 'numberOfSeats',
+	sortable: true,
+	right: true,
+	},
+	{
+	name: 'Start time',
+	selector: 'startDateTime',
+	sortable: true,
+	compact: true,
+	},
+	
 	];
 
 class Events extends Component {
@@ -40,27 +41,32 @@ class Events extends Component {
 		}
 		
 	componentDidMount() {
-	    fetch('/api/v1/event')
-	    .then(res => res.json())
-	    .then((data) => {
-	      this.setState({ events: data })
-	    })
-	    .catch(console.log)
-	  }
+	fetch('/api/v1/event')
+	.then(res => res.json())
+	.then((data) => {
+	this.setState({ events: data })
+	})
+	.catch(console.log)
+	}
 
 	addEvent() {
 		
 	}
 	
-	  render() {
-	    return (
-	      <DataTable
-	        title="Events"
-	        columns={columns2}
-	        data={this.state.events}
-	      />
-	    )
-	  }
+	render() {
+		const tableActions = <Button>New Event</Button>;
+		return (
+				<div>
+					
+						<DataTable
+							title="Events"
+							actions={tableActions}
+							columns={columns2}
+							data={this.state.events}
+						/>
+				</div>
+		)
+	}
 };
 
 export default Events;

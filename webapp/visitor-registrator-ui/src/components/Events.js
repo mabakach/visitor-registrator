cd76cda00b36d59577e59b51895a89c07eae9aa1
Marc Baumgartner <marc@mabaka.ch>
2020-07-26 21:37:58 +0200

    Add row editing

--- webapp/visitor-registrator-ui/src/components/Events.js
+++ webapp/visitor-registrator-ui/src/components/Events.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import {DataTable} from 'primereact/datatable';
 import {Column} from 'primereact/column';
+import { InputText } from 'primereact/inputtext';
 
 class Events extends Component {
+	
 	state = {
-			events: [],
-		}
+		events: [],
+	}
 		
 	componentDidMount() {
 		fetch('/api/v1/event')
@@ -20,22 +22,65 @@ class Events extends Component {
 		
 	}
 	
-	render() {
-		let cols = [
-            {field: 'sysid', header: 'ID'},
-            {field: 'sysinsertts', header: 'Insert timestamp'},
-            {field: 'name', header: 'Event name'},
-            {field: 'numberOfSeats', header: 'Number of Seats'},
-			{field: 'startDateTime', header: 'Start time'}
-
-        ];
-		let dynamicColumns = cols.map((col,i) => {
-            return <Column key={col.field} field={col.field} header={col.header} />;
-        });
+	onEditorValueChange(props, value) {
+	    let updatedEvents = [...this.state.events];
+	    updatedEvents[props.rowIndex][props.field] = value;
+	    this.setState({events: updatedEvents});
+	}
+	
+	nameEditor = (props) => {
+	    return (
+			<InputText type="text" value={this.state.events[props.rowIndex]['name']} onChange={(e) => this.onEditorValueChange(props, e.target.value)} />
+		);
+	}
+	
+	numberOfSeatsEditor = (props) => {
 		return (
-				<DataTable value={this.state.events}>
-					{dynamicColumns}
-				</DataTable>
+			<InputText type="text" keyfilter="pnum" value={this.state.events[props.rowIndex]['numberOfSeats']} onChange={(e) => this.onEditorValueChange(props, e.target.value)}/>	
+		);
+	}
+
+	requiredValidator(props) {
+	    let value = props.rowData[props.field];
+	    return value && value.length > 0;
+	}
+	
+	onRowEditInit(editEvent) {
+	    this.clonedEvents[editEvent.data.sysid] = {...editEvent.data};
+	}
+
+	onRowEditSave(event) {
+	    if (this.onRowEditorValidator(event.data)) {
+	        delete this.clonedCars[event.data.sysid];
+	        // Success message
+	    }
+	    else {
+	        // Error message
+	    }
+	}
+	
+	onRowEditCancel(editEvent) {
+	    let events = [...this.state.events];
+	    events[editEvent.index] = this.clonedEvents[editEvent.data.sysid];
+	    delete this.clonedEvents[editEvent.data.sysid];
+	    this.setState({
+	        events
+	    })
+	}
+	
+	render() {
+		return (
+				<div>
+					<h1>Event Overview</h1>
+					<DataTable value={this.state.events} emptyMessage="No data found" editMode="row">
+						    <Column field="sysid" header="ID" sortable="true"/>
+						    <Column field="sysinsertts" header="Insert timestamp" sortable="true" />
+						    <Column field="name" header="Event name" sortable="true" editor={this.nameEditor} editorValidator={this.requiredValidator}/>
+							<Column field="numberOfSeats" header="Number of Seats" sortable="true" editor={this.numberOfSeatsEditor} editorValidator={this.requiredValidator}/>
+							<Column field="startDateTime" header="Start time" sortable="true" />
+						    <Column rowEditor={true} />
+					</DataTable>
+				</div>
 		)
 	}
 };
